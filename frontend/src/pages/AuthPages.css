e156e484461daa990b39508fb2e965bc1cfecbf2
Nathan Joseph Savio Pereira <98023337+nathanpereira1234@users.noreply.github.com>
2025-08-02 17:07:59 +0530

    Create AuthPages.css

---
+++ frontend/src/pages/AuthPages.css
@@ -0,0 +1,11 @@
+.sketchy-button {
+    border: 2px solid #10B981; /* Green color */
+    border-radius: 9999px; /* Full rounded corners */
+    box-shadow: 3px 3px 0 #10B981;
+    transition: all 0.1s ease-in-out;
+    cursor: pointer;
+}
+.sketchy-button:hover {
+    box-shadow: 1px 1px 0 #10B981;
+    transform: translate(2px, 2px);
+}
