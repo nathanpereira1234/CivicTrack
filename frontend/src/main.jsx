acde876f9601109102652ca4d5371bb4c42202aa
Nathan Joseph Savio Pereira <98023337+nathanpereira1234@users.noreply.github.com>
2025-08-02 17:15:54 +0530

    Create main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ReportIssuePage from './pages/ReportIssuePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import IssueDetailPage from './pages/IssueDetailPage'; // Import the new page
+
+// This component checks if a user is logged in.
+// If not, it redirects them to the login page.
+const PrivateRoute = ({ children }) => {
+  const token = localStorage.getItem('authToken');
+  return token ? children : <Navigate to="/login" />;
+};
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        {/* Protected Routes (User must be logged in to access) */}
+        <Route 
+          path="/" 
+          element={
+            <PrivateRoute>
+              <HomePage />
+            </PrivateRoute>
+          } 
+        />
+        <Route 
+          path="/report" 
+          element={
+            <PrivateRoute>
+              <ReportIssuePage />
+            </PrivateRoute>
+          } 
+        />
+        
+        {/* New route for the Issue Detail Page */}
+        <Route 
+          path="/issue/:id" 
+          element={
+            <PrivateRoute>
+              <IssueDetailPage />
+            </PrivateRoute>
+          } 
+        />
+        
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
