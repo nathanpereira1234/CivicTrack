eb270347cbfee9ddba64c5d5f17f0745ab0385a3
Nathan Joseph Savio Pereira <98023337+nathanpereira1234@users.noreply.github.com>
2025-08-02 17:13:34 +0530

    Create ReportIssuePage.jsx

---
+++ frontend/src/pages/ReportIssuePage.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useMemo, useRef, useEffect } from 'react';
+import { 
+  Container, Typography, TextField, Button, Select, MenuItem, 
+  FormControl, InputLabel, Checkbox, FormControlLabel, Card, CardContent, Box, Grid
+} from '@mui/material';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Icon fix and custom red icon
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+});
+const redIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41], iconAnchor: [12, 41], popupAnchor: [1, -34], shadowSize: [41, 41]
+});
